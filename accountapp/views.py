7b4ea914ad436af685e97d6860f50936ae51008e
TTioTTi <be99999@likelion.org>
2021-12-09 13:58:40 +0900

    hello_world_template update

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-# Function Based Views
+# UI 설정 부분
+def hello_world_template(request):
+    return render(request, 'accountapp/hello_world.html')
+
+
+# 로직 처리 부분
+# API Guide - Views - Function Based Views
 @api_view()
 def hello_world(request):
     return Response({"message": "Hello World"})
