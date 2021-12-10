e3a154141411293640bbfae2b09472cdc453d040
TTioTTi <be99999@likelion.org>
2021-12-10 16:16:39 +0900

    DRF create API View update

--- accountapp/serializers.py
+++ accountapp/serializers.py
@@ -1,8 +1,14 @@
 # django rest framework - API Guide - Declaring Serializers
-
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
 class NewModelSerializer(serializers.Serializer):
     text = serializers.CharField(max_length=255)
     created_at = serializers.DateField()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
