871aaddf9b75568d8ede044176f0563b1c3e99ed
TTioTTi <be99999@likelion.org>
2021-12-13 14:24:54 +0900

    javascript cookie update

--- accountapp/static/js/login.js
+++ accountapp/static/js/login.js
@@ -5,14 +5,20 @@ function send_input() {
         username: document.getElementById('username').value,
         password: document.getElementById('password').value,
       })
+
       .then(function (response) {
         // 성공 했을 경우
         console.log(response);
 
         document.getElementById('alert_box').innerHTML
             = "<div class='btn btn-primary rounded-pill px-5'>로그인이 성공했습니다.</div>";
-        window.location.href = '/accounts/hello_world_template/';
+
+        // Token 수령 후 쿠키 생성
+        // https://cjh5414.github.io/cookie-and-session/#:~:text=%EC%84%B8%EC%85%98%EC%9D%80%20%EC%BF%A0%ED%82%A4%EB%A5%BC%20%EC%9D%B4%EC%9A%A9%ED%95%A9%EB%8B%88%EB%8B%A4.%20%EC%96%B4%EB%96%A4%20%EC%9B%B9%20%EB%B8%8C%EB%9D%BC%EC%9A%B0%EC%A0%80%EA%B0%80%20%EC%84%9C%EB%B2%84%EC%97%90%20%EC%9A%94%EC%B2%AD%EC%9D%84,%EC%BF%A0%ED%82%A4%EC%97%90%20%EC%A0%80%EC%9E%A5%ED%95%B4%EB%91%90%EA%B3%A0%20%EB%A7%A4%20%EC%9A%94%EC%B2%AD%EB%A7%88%EB%8B%A4%20%EC%84%B8%EC%85%98%20%EC%95%84%EC%9D%B4%EB%94%94%EB%A5%BC%20%ED%95%A8
+        // https://ko.javascript.info/cookie
+        document.cookie = "drf_token=Token " + response.data['token'];
       })
+
       .catch(function (error) {
         // 실패 했을 경우
         console.log(error);
