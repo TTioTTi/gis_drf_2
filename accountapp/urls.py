14311ee25bd1dbbb431bb03ae99eb554d17c9140
TTioTTi <be99999@likelion.org>
2021-12-13 11:21:06 +0900

    obtain token view update

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.authtoken import views
 
 from accountapp.views import hello_world, hello_world_template, AccountCreateTemplate, AccountCreateAPIView
 
@@ -9,6 +10,10 @@ urlpatterns = [
     path('hello_world_template/', hello_world_template, name='hello_world_template'),
     # 로직 처리 위한 부분
     path('hello_world/', hello_world, name='hello_world'),
+
+    # https://www.django-rest-framework.org/api-guide/authentication/#by-exposing-an-api-endpoint
+    # By exposing an api endpoint
+    path('login/', views.obtain_auth_token, name='login'),
 
     path('create_template/', AccountCreateTemplate, name='create_template'),
     path('create/', AccountCreateAPIView.as_view(), name='create'),
