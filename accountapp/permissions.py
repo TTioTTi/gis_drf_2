38067705a7a4051eb18db997fe4766d6c7ace379
TTioTTi <be99999@likelion.org>
2021-12-14 15:11:39 +0900

    update API error code update

--- accountapp/permissions.py
+++ accountapp/permissions.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 
 
@@ -6,3 +7,16 @@ class IsOwner(BasePermission):
     def has_permission(self, request, view):
         user = User.objects.get(pk=view.kwargs['pk'])
         return bool(request.user == user)
+
+
+'''
+permissions - IsAdminUser 
+customizing
+
+
+https://www.django-rest-framework.org/api-guide/permissions/#examples
+API Guide - Permissions - Custom permissions - Examples
+
+def has_object_permission(self, request, view, obj):
+    return obj == request.user
+'''
