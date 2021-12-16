1541d3910ccd99a3118832a2f1264f9037d9bc1c
TTioTTi <be99999@likelion.org>
2021-12-16 14:36:46 +0900

    header button error fixed

--- accountapp/static/accountapp/js/token_check.js
+++ accountapp/static/accountapp/js/token_check.js
@@ -26,25 +26,6 @@ function getCookie(name) {
 }
 
 
-var user_id;
-
-axios({
-    method: 'get',
-    url: '/accounts/token/',
-    headers: {
-        Authorization: decodeURIComponent(getCookie('drf_token'))
-    }
-})
-
-.then(function (response) {
-    user_id = response.data['id'];
-    document.getElementById('signup_button').innerHTML =
-    "<a href=\"/accounts/retrieve_template/" + user_id + "\">\n" +
-    "MyPage\n" +
-    "</a>";
-})
-
-
 var check = getCookie('drf_token');
 
 if (check !== undefined) {
@@ -52,4 +33,23 @@ if (check !== undefined) {
         "<a href=\"/accounts/logout_template/\">\n" +
         "Logout\n" +
         "</a>";
+
+
+    var user_id;
+
+    axios({
+        method: 'get',
+        url: '/accounts/token/',
+        headers: {
+            Authorization: decodeURIComponent(getCookie('drf_token'))
+        }
+    })
+
+    .then(function (response) {
+        user_id = response.data['id'];
+        document.getElementById('signup_button').innerHTML =
+        "<a href=\"/accounts/retrieve_template/" + user_id + "\">\n" +
+        "MyPage\n" +
+        "</a>";
+    })
 }
